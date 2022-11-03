aee1ed681511e233995880d0b83ad52a71b4a682
Doom-Art <ouginak121@gmail.com>
2022-11-03 08:59:52 -0400

    added pac

--- Game1.cs
+++ Game1.cs
@@ -8,11 +8,14 @@ namespace Monogame_Lesson_1_The_Basics
     public class Game1 : Game
     {
         Texture2D dinoTexture;
+        Texture2D pacTexture;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private static Random rand = new Random();
-        private int _x = 0;
+        private int _x = 150;
         private int _y = 0;
+        private int _xPac = 0;
+        private int _yPac = 0;
 
         public Game1()
         {
@@ -24,9 +27,9 @@ namespace Monogame_Lesson_1_The_Basics
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            this.Window.Title = "Test console";
+            this.Window.Title = "Pac chasing Ghost";
             _graphics.PreferredBackBufferWidth = 800; // Sets the width of the window
-            _graphics.PreferredBackBufferHeight = 800; // Sets the height of the window
+            _graphics.PreferredBackBufferHeight = 700; // Sets the height of the window
             _graphics.ApplyChanges(); // Applies the new dimensions
             base.Initialize();
         }
@@ -36,13 +39,22 @@ namespace Monogame_Lesson_1_The_Basics
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            dinoTexture = Content.Load<Texture2D>("dino2");
+            dinoTexture = Content.Load<Texture2D>("pacGhost");
+            pacTexture = Content.Load<Texture2D>("pac");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && _xPac < _graphics.PreferredBackBufferWidth-100)
+                _xPac+=5;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && _xPac > 0)
+                _xPac-=5;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) && _yPac < _graphics.PreferredBackBufferHeight-100)
+                _yPac+=5;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && _yPac > 0)
+                _yPac-=5;
 
             // TODO: Add your update logic here
 
@@ -56,15 +68,14 @@ namespace Monogame_Lesson_1_The_Basics
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(dinoTexture, new Vector2(_x, _y), Color.White);
+            _spriteBatch.Draw(pacTexture, new Vector2(_xPac, _yPac), Color.White);
             _spriteBatch.End();
             _x+=5;
-            if (_x >= _graphics.PreferredBackBufferWidth)
-            {
+            if (_x >= _graphics.PreferredBackBufferWidth){
                 _x = 0;
                 _y += 150;
             }
-            if (+_y >= _graphics.PreferredBackBufferHeight)
-            {
+            if (+_y >= _graphics.PreferredBackBufferHeight){
                 _y = 0;
             }
 
